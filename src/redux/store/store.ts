0dd19a6444b92d54d44c8f0921f690eaa8ec49f9
glitch-prog <itgeniusofblr@gmail.com>
2022-06-14 19:56:33 +0300

    init commit

---
+++ src/redux/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: myCustomApiService,
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
